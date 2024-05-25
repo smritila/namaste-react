b2cdb2a604740dd2339921f2bd16d6e54700680a
Smriti <smrtghoshal24@gmail.com>
2024-05-25 22:06:07 +0530

    chore: Initial commit.

--- App.js
+++ App.js
@@ -13,10 +13,12 @@
  * ReactElement(Object) => HTML(Browser Understands)
  */
 
+import React from "react";
+import ReactDOM from "react-dom/client";
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm a h1 tag"),
-    React.createElement("h2", {}, "I'm a h2 tag"),
+    React.createElement("h1", {}, "This is Namaste React 🚀"),
+    React.createElement("h2", {}, "by Akshay Saini"),
   ]),
   React.createElement("div", { id: "child2" }, [
     React.createElement("h1", {}, "I'm a h1 tag"),
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# namaste React
+# Namaste React🚀
+
+#Parcel
+-Dev Build
+-Local Server
+-HMR- Hote Module Replacement
+-File watching algorithm-Written in C++
+-caching-Faster builds
+-Image optimization
+-Minification
+-bundling
+-Compress
+-consistent Hashing
+-code splitting
+-Differential bundling
+-support older browsers
+-Diagnostic
+-Error handling
+o
