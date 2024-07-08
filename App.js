8ee137d37431fc101148268ff78b3dbea3f4a32e
Smriti <smrtghoshal24@gmail.com>
2024-07-08 18:30:24 +0530

    chore: "Small changes in app.js"

--- App.js
+++ App.js
@@ -1,2 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+//React. createElement=> object=> HTMLElement(render)
+const heading = React.createElement("h1", { id: "heading" }, "Namaste React");
+console.log(heading);
+//JSX (transpiled before it reaches the JS)
+// not HTML in javascript. // HTML like  or XML - like syntax.
+const jsxHeading = <h1 id="heading">Namaste React Using JSX </h1>;
+console.log(jsxHeading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(jsxHeading);
